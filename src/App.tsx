3bfe08f5e1ac1e0fccfa452e5f8cddea8f49491d
Natan Matos <njmatos21@gmail.com>
2024-11-11 12:43:42 -0300

    paginar e filtrar itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from 'react'
 import Select from './components/select'
 
-export type Todo = {
-  userId: number
-  id: number
-  title: string
-  completed: boolean
-}
-
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10')
-      .then((response) => response.json())
-      .then((json) => setTodos(json))
-  }, [])
-
   return (
-    <div className="w-[400px]">
-      <Select data={todos} />
+    <div className="bg-[#f7f8ff] w-[400px]">
+      <Select url="https://jsonplaceholder.typicode.com/todos" />
     </div>
   )
 }
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,31 +1,108 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
-type ItemProps = {
+export type Todo = {
+  userId: number
   id: number
   title: string
+  completed: boolean
 }
 
 type SelectProps = {
-  data: ItemProps[]
+  url: string
 }
 
-function Select({ data, ...props }: SelectProps) {
-  const [selectedTodo, setSelectedTodo] = useState<ItemProps | null>(null)
+function Select({ url }: SelectProps) {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([])
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null)
+  const [start, setStart] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [finished, setFinished] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
 
-  window.parent.postMessage({ name: 'Select option', data: selectedTodo }, '*')
+  const todosRef = useRef<HTMLDivElement>(null)
+  const prevScrollY = useRef(0)
+
+  const handleScroll = () => {
+    if (todosRef.current) {
+      const { scrollHeight, scrollTop, clientHeight } = todosRef.current
+      const isScrollingDown = scrollTop > prevScrollY.current
+      prevScrollY.current = scrollTop
+
+      if (isScrollingDown && scrollHeight - 300 < scrollTop + clientHeight) {
+        setLoading(true)
+      }
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${url}?_start=${start}&_limit=10`)
+      const data = await response.json()
+      const updatedTodos = [...todos, ...data]
+      setTodos(updatedTodos)
+      setFilteredTodos(updatedTodos)
+      setLoading(false)
+
+      if (!data.length) {
+        setFinished(true)
+      }
+    } catch {
+      setLoading(false)
+    }
+  }
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const term = event.target.value
+    setSearchTerm(term)
+    const filtered = todos.filter((todo) =>
+      todo.title.toLowerCase().includes(term.toLowerCase())
+    )
+    setFilteredTodos(filtered)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [start])
+
+  useEffect(() => {
+    if (loading && !finished) {
+      setStart((prevPage) => prevPage + 10)
+    }
+  }, [loading, finished])
+
+  useEffect(() => {
+    if (todosRef.current) {
+      todosRef.current.addEventListener('scroll', handleScroll)
+      return () => {
+        todosRef.current?.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [todosRef])
+
+  window.parent.postMessage(
+    { name: 'Select option', data: JSON.stringify(selectedTodo) },
+    '*'
+  )
 
   return (
     <>
       <input
-        placeholder="Digite aqui"
-        className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-        {...props}
+        placeholder="Buscar por item"
+        className="flex h- w-full rounded-md border border-input px-2 py-4 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+        value={searchTerm}
+        onChange={handleSearch}
       />
 
-      <div className="flex gap-2 flex-wrap justify-end mt-4">
-        {data.map((item) => (
+      <div
+        className="flex gap-2 flex-wrap items-start justify-end mt-4 h-[300px] overflow-y-auto"
+        ref={todosRef}
+      >
+        {filteredTodos.map((item) => (
           <div
-            className="p-2 bg-blue-500 text-white rounded-lg hover:cursor-pointer hover:bg-blue-600"
+            className={`p-2 ${
+              selectedTodo?.id === item.id ? 'bg-blue-600' : 'bg-blue-500'
+            } text-white rounded-lg hover:cursor-pointer hover:bg-blue-600`}
             key={item.id}
             onClick={() => setSelectedTodo(item)}
           >
